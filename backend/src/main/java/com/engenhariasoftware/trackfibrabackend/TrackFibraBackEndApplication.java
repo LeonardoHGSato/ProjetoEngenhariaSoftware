5101cc7e5e45e5115d12bac46ef8dffef9d9fe24
Leonardo Sato <leonardosato29@gmail.com>
2026-05-27 11:19:53 -0300

    chore: adiciona estrutura inical e dependências do backend

---
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/TrackFibraBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.engenhariasoftware.trackfibrabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFibraBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackFibraBackEndApplication.class, args);
+    }
+
+}
