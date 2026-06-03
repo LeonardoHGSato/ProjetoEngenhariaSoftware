3cbc9d2e91d63876168be6eace14ac86a25ca440
Guilherme Fiuza <guifportela@gmail.com>
2026-06-03 13:18:32 -0300

    feat: adiciona consultas por status no repository e metodos de listar e buscar no service

--- backend/src/main/java/com/engenhariasoftware/trackfibrabackend/repository/CarroRepository.java
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/repository/CarroRepository.java
@@ -1,9 +1,15 @@
 package com.engenhariasoftware.trackfibrabackend.repository;
 
+import com.engenhariasoftware.trackfibrabackend.enums.StatusCarro;
 import com.engenhariasoftware.trackfibrabackend.model.Carro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarroRepository extends JpaRepository<Carro, Long> {
 
     boolean existsByPlaca(String placa);
+
+    Page<Carro> findByStatus(StatusCarro status, Pageable pageable);
+    Page<Carro> findByStatusNot(StatusCarro status, Pageable pageable);
 }
--- backend/src/main/java/com/engenhariasoftware/trackfibrabackend/service/CarroService.java
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/service/CarroService.java
@@ -3,8 +3,11 @@ package com.engenhariasoftware.trackfibrabackend.service;
 
 import com.engenhariasoftware.trackfibrabackend.dto.CarroRequestDTO;
 import com.engenhariasoftware.trackfibrabackend.dto.CarroResponseDTO;
+import com.engenhariasoftware.trackfibrabackend.enums.StatusCarro;
 import com.engenhariasoftware.trackfibrabackend.model.Carro;
 import com.engenhariasoftware.trackfibrabackend.repository.CarroRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -43,6 +46,38 @@ public class CarroService {
                 carroSalvo.getMarca(),
                 carroSalvo.getAno(),
                 carroSalvo.getStatus()
+        );
+    }
+
+    public Page<CarroResponseDTO> listar(StatusCarro status, Pageable pageable){
+        Page<Carro> paginaDeCarros;
+
+        if(status != null){
+            paginaDeCarros = carroRepository.findByStatus(status, pageable);
+        } else {
+            paginaDeCarros = carroRepository.findByStatusNot(StatusCarro.DESATIVADO, pageable);
+        }
+
+        return paginaDeCarros.map(carro -> new CarroResponseDTO(
+                carro.getId(),
+                carro.getPlaca(),
+                carro.getModelo(),
+                carro.getMarca(),
+                carro.getAno(),
+                carro.getStatus()
+        ));
+    }
+
+    public CarroResponseDTO buscarPorId(Long id){
+
+        Carro carro = carroRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Carro não encontrado"));
+        return new CarroResponseDTO(
+                carro.getId(),
+                carro.getPlaca(),
+                carro.getModelo(),
+                carro.getMarca(),
+                carro.getAno(),
+                carro.getStatus()
         );
     }
 }
