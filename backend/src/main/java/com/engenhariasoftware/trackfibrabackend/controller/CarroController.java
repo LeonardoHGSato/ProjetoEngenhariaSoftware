98da5d1512463ac6fa67155ef0fac490ffed06a1
Guilherme Fiuza <guifportela@gmail.com>
2026-06-03 13:58:24 -0300

    feat: adiciona rotas GET no CarroController para listar e buscar carros por id

--- backend/src/main/java/com/engenhariasoftware/trackfibrabackend/controller/CarroController.java
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/controller/CarroController.java
@@ -2,14 +2,14 @@ package com.engenhariasoftware.trackfibrabackend.controller;
 
 import com.engenhariasoftware.trackfibrabackend.dto.CarroRequestDTO;
 import com.engenhariasoftware.trackfibrabackend.dto.CarroResponseDTO;
+import com.engenhariasoftware.trackfibrabackend.enums.StatusCarro;
 import com.engenhariasoftware.trackfibrabackend.service.CarroService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/carros")
@@ -25,5 +25,16 @@ public class CarroController {
     public ResponseEntity<CarroResponseDTO> cadastrar(@RequestBody @Valid CarroRequestDTO dto){
         CarroResponseDTO recibo = carroService.cadastrar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(recibo);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CarroResponseDTO>> listar(
+            @RequestParam(required = false)StatusCarro status, Pageable pageable){
+        return ResponseEntity.ok(carroService.listar(status, pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CarroResponseDTO> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(carroService.buscarPorId(id));
     }
 }
