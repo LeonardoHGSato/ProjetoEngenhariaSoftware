d1f95a288d05eda11a14564bfc9d79cdee19eaff
Leonardo Sato <leonardosato29@gmail.com>
2026-06-12 18:54:11 -0300

    feat: adiciona validacao aos DTOs de FuncionarioRequest e FuncionarioEdicao

--- backend/src/main/java/com/engenhariasoftware/trackfibrabackend/dto/FuncionarioEdicaoDTO.java
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/dto/FuncionarioEdicaoDTO.java
@@ -2,6 +2,8 @@ package com.engenhariasoftware.trackfibrabackend.dto;
 
 import com.engenhariasoftware.trackfibrabackend.enums.PerfilFuncionario;
 import com.engenhariasoftware.trackfibrabackend.enums.StatusFuncionario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,8 +15,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FuncionarioEdicaoDTO {
+
+    @NotBlank(message = "O nome é obrigatório.")
     private String nome;
+
+    @NotBlank(message = "O nome é obrigatório.")
+    @Email(message = "Formato de email inválido")
     private String email;
+
 //    Validacao de numero de telefone baseada no tamanho dele e se há apenas números
     @Pattern(regexp = "^\\d{11}$", message = "Telefone inválido. Use apenas números com DDD (11 dígitos)")
     private String numeroTelefone;
--- backend/src/main/java/com/engenhariasoftware/trackfibrabackend/dto/FuncionarioRequestDTO.java
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/dto/FuncionarioRequestDTO.java
@@ -1,6 +1,8 @@
 package com.engenhariasoftware.trackfibrabackend.dto;
 
 import com.engenhariasoftware.trackfibrabackend.validation.CpfValido;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,8 +14,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FuncionarioRequestDTO {
+    @NotBlank(message = "O nome é obrigatório.")
     private String nome;
+
+    @NotBlank(message = "O nome é obrigatório.")
+    @Email(message = "Formato de email inválido")
     private String email;
+
+    @NotBlank(message = "O nome é obrigatório.")
     private String senha;
     //    Validacao de numero de telefone baseada no tamanho dele e se há apenas números
     @Pattern(regexp = "^\\d{11}$", message = "Telefone inválido. Use apenas números com DDD (11 dígitos)")
