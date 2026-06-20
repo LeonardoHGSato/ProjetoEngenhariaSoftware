41dafa6eb2a796dcade74c3a94fc82ef6a20571c
Leonardo Sato <leonardosato29@gmail.com>
2026-06-20 17:21:29 -0300

    feat: adiciona configuração de CORS

--- backend/src/main/java/com/engenhariasoftware/trackfibrabackend/config/SecurityConfig.java
+++ backend/src/main/java/com/engenhariasoftware/trackfibrabackend/config/SecurityConfig.java
@@ -14,44 +14,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor // Injeta automaticamente o seu filtro JWT por construtor
+@RequiredArgsConstructor // Injeta automaticamente o filtro JWT por construtor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
-    private final UserDetailsService userDetailsService; // Interface do Spring para buscar usuários
+    private final UserDetailsService userDetailsService;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // 1. Desativa CSRF porque API REST com JWT não armazena estado em cookies
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
 
-                // 2. Configura as permissões de rota alinhadas com o backlog
                 .authorizeHttpRequests(auth -> auth
-                        // Rota de login pública para qualquer um tentar se autenticar
                         .requestMatchers("/api/v1/auth/login").permitAll()
-
-                        // CRUD de Funcionários restrito a SUPERVISOR (Critério da Issue 3)
                         .requestMatchers("/api/v1/funcionarios/**").hasRole("SUPERVISOR")
-
-                        // Qualquer outra rota do sistema exigirá que o usuário esteja logado
                         .anyRequest().authenticated()
                 )
 
-                // 3. Define a sessão como STATELESS (sem estado no servidor)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
-                // 4. Configura o provedor de autenticação customizado
                 .authenticationProvider(authenticationProvider())
-
-                // 5. Adiciona o seu filtro JWT na fila de execução, ANTES do filtro de usuário/senha padrão
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
