a16f59d56e89082c3e9411ff518f73634d1a4e1d
Guilherme Fiuza <guifportela@gmail.com>
2026-06-22 14:05:34 -0300

    chore: configura infraestrutura base de testes com JUnit e H2

--- backend/src/test/java/com/engenhariasoftware/trackfibrabackend/TrackFibraBackEndApplicationTests.java
+++ backend/src/test/java/com/engenhariasoftware/trackfibrabackend/TrackFibraBackEndApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TrackFibraBackEndApplicationTests {
+class TrackFibraBackEndApplicationTests extends BaseTest{
 
     @Test
     void contextLoads() {
